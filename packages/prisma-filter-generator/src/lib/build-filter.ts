ded54be2f5f3b434a17009d898e078d5f5930ceb
baijanathTharu <bnchaudhary.tharu@gmail.com>
2023-07-25 21:37:15 +0545

    create test cases and use interfaces

--- packages/prisma-filter-generator/src/lib/build-filter.ts
+++ packages/prisma-filter-generator/src/lib/build-filter.ts
@@ -1,4 +1,27 @@
-export function buildFilter(filter: any) {
+export interface IFilter {
+  /**
+   * field (optional): The field on which the filtering will be performed.
+   */
+  field?: string;
+  /**
+   * operator (optional): The comparison operator used for filtering the field.
+   */
+  operator?: 'gte' | 'lte' | 'equals' | 'contains';
+  /**
+   * value (optional): The value against which the field will be compared using the specified operator.
+   */
+  value?: any;
+  /**
+   * logicalOperator (optional): Specifies the logical operator used to combine multiple filters. It can be either 'AND' or 'OR'.
+   */
+  logicalOperator?: 'AND' | 'OR';
+  /**
+   * filters (optional): An array of nested IFilter objects used to create complex filter conditions.
+   */
+  filters?: IFilter[];
+}
+
+export function buildFilter(filter: IFilter) {
   if (!filter) return {};
 
   const where: any = {};
@@ -24,93 +47,17 @@ export function buildFilter(filter: any) {
     }
   }
 
-  if (filter.logicalOperator && filter.filters?.length > 0) {
-    const nestedConditions = filter.filters.map(buildFilter);
+  if (filter.filters) {
+    if (filter.logicalOperator && filter.filters?.length > 0) {
+      const nestedConditions = filter.filters.map(buildFilter);
 
-    if (filter.logicalOperator === 'AND') {
-      where.AND = nestedConditions;
-    } else if (filter.logicalOperator === 'OR') {
-      where.OR = nestedConditions;
+      if (filter.logicalOperator === 'AND') {
+        where.AND = nestedConditions;
+      } else if (filter.logicalOperator === 'OR') {
+        where.OR = nestedConditions;
+      }
     }
   }
 
   return where;
 }
-
-// const filter1 = {
-//   logicalOperator: 'OR',
-//   filters: [
-//     {
-//       field: 'author.name',
-//       operator: 'contains',
-//       value: 'John',
-//     },
-//     {
-//       logicalOperator: 'OR',
-//       filters: [
-//         {
-//           field: 'author.age',
-//           operator: 'gte',
-//           value: 30,
-//         },
-//         {
-//           logicalOperator: 'AND',
-//           filters: [
-//             {
-//               field: 'author.createdAt',
-//               operator: 'gte',
-//               value: '2022-01-01',
-//             },
-//             {
-//               field: 'author.createdAt',
-//               operator: 'lte',
-//               value: '2022-02-01',
-//             },
-//           ],
-//         },
-//       ],
-//     },
-//   ],
-// };
-
-/**
-// find users from country Nepal with at least one published post
-
- - a post can have multiple authors.
- - an author can write many posts.
-
- prisma.user.findMany({
-  where: {
-    country: {
-      name: {
-        equals: "Nepal"
-      }
-    },
-    posts: {
-      some: {
-        isPublished: {
-          equals: true
-        }
-      }
-    }
-  }
- })
- */
-const filter1 = {
-  logicalOperator: 'AND',
-  filters: [
-    {
-      field: 'country.id.not',
-      operator: 'equals',
-      value: 'Nepal',
-    },
-    {
-      field: 'posts.some.isPublished',
-      operator: 'equals',
-      value: true,
-    },
-  ],
-};
-
-const generatedFilter = buildFilter(filter1);
-console.log('generatedFilter', JSON.stringify(generatedFilter, null, 2));
